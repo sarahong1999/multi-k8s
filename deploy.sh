cd6733e87bb2e8e25425654588dd82a39a752bd3
sarahong1999 <csc940218@gmail.com>
2022-11-20 18:32:32 +0800

    change dockerhub

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t sarah19990/multi-client:latest -t sarah19990/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t sarah19990/multi-server:latest -t sarah19990/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t sarah19990/multi-worker:latest -t sarah19990/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t csc940218/multi-client:latest -t csc940218/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t csc940218/multi-server:latest -t csc940218/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t csc940218/multi-worker:latest -t csc940218/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push sarah19990/multi-client:latest
-docker push sarah19990/multi-server:latest
-docker push sarah19990/multi-worker:latest
+docker push csc940218/multi-client:latest
+docker push csc940218/multi-server:latest
+docker push csc940218/multi-worker:latest
 
-docker push sarah19990/multi-client:$SHA
-docker push sarah19990/multi-server:$SHA
-docker push sarah19990/multi-worker:$SHA
+docker push csc940218/multi-client:$SHA
+docker push csc940218/multi-server:$SHA
+docker push csc940218/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=sarah19990/multi-server:$SHA
-kubectl set image deployments/client-deployment client=sarah19990/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=sarah19990/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=csc940218/multi-server:$SHA
+kubectl set image deployments/client-deployment client=csc940218/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=csc940218/multi-worker:$SHA
